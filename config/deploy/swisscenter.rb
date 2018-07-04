c2011c8ffaabffeab90b09b3538690a9f2ab9937
Julien Ramel <julien.ramel@gmail.com>
2018-07-04 10:15:38 +0200

    new speakers

--- config/deploy/swisscenter.rb
+++ config/deploy/swisscenter.rb
@@ -15,7 +15,7 @@ server "94.103.96.180", user: 'terolabs', roles: %w{web app db}
 
 
 
-SSHKit.config.command_map[:composer] = "/usr/local/php55/bin/php /home/terolabs/utils/php/composer/composer.phar"
+SSHKit.config.command_map[:composer] = "php56 /home/terolabs/utils/php/composer/composer.phar"
 
 SSHKit.config.command_map[:wp] ="~/bin/wp.sh"
 
